6ba6ee996e3a64bfab613f69f949fe02e1cc42a5
Connor Feeney <connor.w.feeney@gmail.com>
2026-03-16 20:44:59 -0700

    Added pyproject.toml

--- tests/python/conftest.py
+++ tests/python/conftest.py
@@ -3,6 +3,6 @@ from pathlib import Path
 
 
 ROOT = Path(__file__).resolve().parents[2]
-PYTHON_MODULE_DIR = ROOT / "python" / "dshm"
+PYTHON_MODULE_DIR = ROOT / "build" / "debug" / "lib"
 
 sys.path.insert(0, str(PYTHON_MODULE_DIR))
